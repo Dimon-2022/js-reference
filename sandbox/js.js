9e0d7f16984fd539088ff591e76b508b174174b2
dima.moisieienko <dimonmail09@gmail.com>
2025-02-09 14:24:39 +0200

    methods of arrays

---
+++ sandbox/js.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const fruits = ['orange', 'peach', 'plum', 'strawberry', 'tangerine'];
+
+//добовляет элемент в конец массива
+// fruits.push('pear');
+// fruits.push('apricot');
+
+// //добовляет элемент в начало массива
+// fruits.unshift('kivi');
+
+
+// //удаляет элемент в конце массива
+// fruits.pop();
+
+// //удаляет элемент в начале массива
+// fruits.shift();
+
+
+
+
+
+// let x = fruits.splice(1, 3);
+// console.log(x);
+
+
+// console.log(fruits.includes('peach'));
+
+console.log(fruits.indexOf('peach'));
+
+console.log(fruits);
