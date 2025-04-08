3f3a3c630b91d840af93ebd7c65df0ab94348088
dima.moisieienko <dimonmail09@gmail.com>
2025-04-08 19:30:08 +0300

    flag API

--- 23-asynchronous-code/start/script.js
+++ 23-asynchronous-code/start/script.js
@@ -3,14 +3,50 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
-///////////////////////////////////////
+const input = document.querySelector('.required-country');
 
-const request = new XMLHttpRequest();
-request.open('GET', 'https://meowfacts.herokuapp.com/?lang=ukr-ua');
-request.send();
-request.addEventListener('load', function(){
-    const data = JSON.parse(request.responseText);
-    const [text] = data.data;
-    console.log(text);
-    document.querySelector('.container').textContent = text;
-})
+///////////////////////////////////////
+btn.addEventListener('click', function () {
+  console.log(input.value);
+
+  if (input.value) {
+    const request = new XMLHttpRequest();
+    request.open(
+      'GET',
+      `https://restcountries.com/v3.1/name/${input.value.toLowerCase()}`
+    );
+    request.send();
+
+    request.addEventListener('load', function () {
+      const data = JSON.parse(request.responseText);
+
+      const [countryObj] = data;
+      console.log(countryObj);
+      renderCountry(countryObj);
+    });
+  }
+});
+
+function renderCountry(countryObj) {
+  const { currencies } = countryObj;
+
+  let curr = [];
+
+  for (let key in currencies) {
+    curr.push(currencies[key].name);
+  }
+
+  let languages = Object.values(countryObj.languages);
+
+  const html = `<article class="country">
+    <img class="country__img" src="${countryObj.flags.png}" />
+    <div class="country__data">
+      <h3 class="country__name">${countryObj.name.official}</h3>
+      <h4 class="country__region">${countryObj.region}</h4>
+      <p class="country__row"><span>👫</span>${countryObj.population}</p>
+      <p class="country__row"><span>🗣️</span>${languages}</p>
+      <p class="country__row"><span>💰</span>${curr.join(', ')}</p>
+    </div>
+  </article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+}
